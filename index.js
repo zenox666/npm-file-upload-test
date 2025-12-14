809e8f143ee831139b6624c7ff0aea3c081748dc
Allu Ketan Satya Sai <allusai2007@gmail.com>
2025-12-14 18:08:01 +0530

    Add files via upload
    initial upload

---
+++ index.js
@@ -0,0 +1,6 @@
+import generateName from "sillyname";
+var sillyname = generateName();
+console.log(`My name is ${sillyname}`);
+
+import {randomSuperhero} from 'superheroes';
+console.log(`I am ${randomSuperhero()}`);
